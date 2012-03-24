68d6dc65c3f2c681536b0b64516d4192f8511873
maty974 <matthieucadet+github@gmail.com>
2012-03-24 12:04:23 +0100

    added label knob tcl python code to show connected Input and Output nodes name

--- nuke_python_snippet.py
+++ nuke_python_snippet.py
@@ -3,5 +3,11 @@ Created on Mar 24, 2012
 
 @author: matthieu cadet
 @contact: matthieu.cadet+coding@gmail.com
-@summary: 
+@summary: Some code snippet for The Foundry Nuke software
 '''
+
+# Used in label knob, list all Input Nodes name connected
+[python {"\n".join(["input: %s" % node.name() for node in nuke.thisNode().dependencies()])}]
+
+# Used in label knob, list all Output Nodes name connected
+[python {"\n".join(["output: %s" % node.name() for node in nuke.thisNode().dependent()])}]
