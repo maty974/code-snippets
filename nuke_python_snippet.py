8875e5367da4190729e561ba22e230849d84df1c
maty974 <matthieucadet+github@gmail.com>
2012-03-24 12:26:02 +0100

    removed all tcl snippet, must contain only pure python code

--- nuke_python_snippet.py
+++ nuke_python_snippet.py
@@ -5,9 +5,3 @@ Created on Mar 24, 2012
 @contact: matthieu.cadet+coding@gmail.com
 @summary: Some code snippet for The Foundry Nuke software
 '''
-
-# Used in label knob, list all Input Nodes name connected
-[python {"\n".join(["input: %s" % node.name() for node in nuke.thisNode().dependencies()])}]
-
-# Used in label knob, list all Output Nodes name connected
-[python {"\n".join(["output: %s" % node.name() for node in nuke.thisNode().dependent()])}]
